688835a94f7165538122a6d228f89c149aab0b2a
Judith Mueni <judithmueni395@gmail.com>
2018-03-06 08:53:49 +0300

    Names file

---
+++ Names.py
@@ -0,0 +1,18 @@
+students= ["Judith", "Assupmter","Judy", "Evelyne", "Julia", "Farida", "Ivy", "Taspin", "Leah", "Gift"]
+for i in range(10):
+  print("What is the name of student {} : {}".format(i , students[i]))
+
+  #name strings
+
+  students= ["Judith", "Assupmter","Judy", "Evelyne", "Julia", "Farida", "Ivy", "Taspin", "Leah", "Gift"]
+welcome_str= "welcome Ms {} to Backened class"
+print (welcome_str.format(students[0]))
+print (welcome_str.format(students[1]))
+print (welcome_str.format(students[2]))
+print (welcome_str.format(students[3]))
+print (welcome_str.format(students[4]))
+print (welcome_str.format(students[5]))
+print (welcome_str.format(students[6]))
+print (welcome_str.format(students[7]))
+print (welcome_str.format(students[8]))
+print (welcome_str.format(students[9]))
